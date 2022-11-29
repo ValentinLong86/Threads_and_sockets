04f3704354adbae1cac62925fec97c72959d2df8
Valentin <valentin.long86@gmail.com>
2022-11-29 19:16:00 +0100

    chat_syncrone_sans_while_true

--- Sockets_client-serveur/chat_syncrone/client.py
+++ Sockets_client-serveur/chat_syncrone/client.py
@@ -6,25 +6,27 @@ port = 7777
 
 def socket_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
-            try:
-                client.connect((host, port))
-            except ConnectionRefusedError:
-                print("Le serveur n'accepte pas la connexion ou est indisponible")
-            except:
-                print("Erreur de communication avec le serveur ")
-            else:
-                while True:
-                    send_message = input("CLIENT> ")
-                    client.send(send_message.encode("utf-8"))
-                    
-                    if send_message == "bye" or send_message == "arret":
-                        client.close()
-                        sys.exit(0)
+        try:
+            client.connect((host, port))
+        except ConnectionRefusedError:
+            print("Le serveur n'accepte pas la connexion ou est indisponible")
+            sys.exit(0)
+        except:
+            print("Erreur de communication avec le serveur ")
+            sys.exit(0)
 
-                    buffer = client.recv(1024)
-                    rc_message = buffer.decode("utf-8")
+        message = ""
 
-                    print("RECEIVE:", rc_message)
+        while message != "arret" and message != "bye":
+            message = input("SEND> ")
+            client.send(message.encode("utf-8"))
+
+            buffer = client.recv(1024)
+            receive_message = buffer.decode("utf-8")
+
+            print("RECEIVE:", receive_message)
+
+        client.close()
 
 
 if __name__ == "__main__":
--- Sockets_client-serveur/chat_syncrone/server.py
+++ Sockets_client-serveur/chat_syncrone/server.py
@@ -1,5 +1,4 @@
 import socket
-import sys
 
 host = "127.0.0.1"
 port = 7777
@@ -8,23 +7,26 @@ def socket_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         server.bind((host, port))
         server.listen(1)
+        receive_message = ""
 
-        while True:
+        while receive_message != "arret":
+            receive_message = ""
             conn, address = server.accept()
-            while True:
-                buffer = conn.recv(1024)
-                message = buffer.decode("utf-8")
+            
+            while receive_message != "arret" and receive_message != "bye":
+                buff = conn.recv(1024)
+                receive_message = buff.decode("utf-8")
 
-                print("RECEIVE:", message)
-                    
-                if message == "arret":
-                    server.close()
-                    sys.exit(0)
-                elif message == "bye":
-                    break
+                if receive_message == "arret" or receive_message == "bye":
+                    conn.send(receive_message.encode("utf-8"))
                 
-                send_message = input("SERVER> ")
-                conn.send(send_message.encode("utf-8"))
+                else:
+                    print("RECEIVE:", receive_message)
+                    
+                    message = input("SEND> ")
+                    conn.send(message.encode("utf-8"))
+        
+        server.close()
 
 
 if __name__ == "__main__":
